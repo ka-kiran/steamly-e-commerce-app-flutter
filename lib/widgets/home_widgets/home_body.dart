fc05c24722a12059d28367e1005eb97d342d9206
Kiran Khadka <khadka.kiran1110@gmail.com>
2021-08-08 14:13:20 +0545

    product model data list added

--- lib/widgets/home_widgets/home_body.dart
+++ lib/widgets/home_widgets/home_body.dart
@@ -20,17 +20,83 @@ class HomePageBody extends StatelessWidget {
               size:
                   size), // extracted widget accessed from home_widgets/header_with_searchbox.dart
           Container(
+            child:
+                NavigationBelowSearch(), // widget for navigation section below the search box
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
             height: 24,
             child: Stack(
-              children:<Widget> [
-                Text("Recommended", style: TextStyle(
-                  fontFamily: GoogleFonts.roboto().fontFamily,
-                  fontSize: 16.0,
-                ),)
+              children: <Widget>[
+                Text(
+                  "Recommended",
+                  style: TextStyle(
+                    fontFamily: GoogleFonts.roboto().fontFamily,
+                    fontSize: 16.0,
+                    color: mainTextColor,
+                  ),
+                ),
+                Container(),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+// navigation section below the search box widget
+class NavigationBelowSearch extends StatefulWidget {
+  const NavigationBelowSearch({Key? key}) : super(key: key);
+
+  @override
+  _NavigationBelowSearchState createState() => _NavigationBelowSearchState();
+}
+
+class _NavigationBelowSearchState extends State<NavigationBelowSearch> {
+  List<String> navSection = [
+    "Popular",
+    "New Arrivals",
+    "All Categories",
+    "Offers",
+    "Luxury",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30.0,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal, // scroll towards horizontal direction
+        itemCount: navSection.length,
+        itemBuilder: (context, index) => buildNavSection(index),
+      ),
+    );
+  }
+
+// widget to build navigation section below search box
+  Widget buildNavSection(int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      // Container widget with box decorated microinteraction nav section
+      child: Container(
+        height: 20.0,
+        padding: EdgeInsets.only(left: 20.0, right: 20.0),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Text(
+          navSection[index], // pass navigation bar list
+          style: TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 12.0,
+          ),
+        ),
       ),
     );
   }
